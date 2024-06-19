a8e862d0035d5dfa10f7bedec45662bca2a2d9f3
MeVishnupriya <vishnupriya.singamaneni@gmail.com>
2024-06-19 15:17:50 -0400

    Initial commit for social media blog app

---
+++ social-media-blog-app-crud/src/main/java/com/learning/springboot/social_media_blog_app_crud/SocialMediaBlogAppCrudApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.social_media_blog_app_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaBlogAppCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaBlogAppCrudApplication.class, args);
+	}
+
+}
---
+++ social-media-blog-app-crud/src/test/java/com/learning/springboot/social_media_blog_app_crud/SocialMediaBlogAppCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springboot.social_media_blog_app_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBlogAppCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
